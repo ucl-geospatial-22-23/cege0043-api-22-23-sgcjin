553c8d556b963d656b6292b26dacba1319189279
sgcjin <sgcjin@gmail.com>
2023-02-10 15:00:21 +0000

    add CORS request and log

--- dataAPI.js
+++ dataAPI.js
@@ -10,5 +10,21 @@ let http = require('http');
 let httpServer = http.createServer(app);
 httpServer.listen(4480);
 app.get('/',function (req,res) {
-res.send("hello world from the Data API "+ now());
+res.send("hello world from the Data API ");
+});
+
+// adding functionality to allow cross-origin queries
+app.use(function(req, res, next) {
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+next();
+});
+
+// adding functionality to log the requests
+app.use(function (req, res, next) {
+let filename = path.basename(req.url);
+let extension = path.extname(filename);
+console.log("The file " + filename + " was requested.");
+next();
 });
